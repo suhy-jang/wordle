27139055ce50141ed4c7e9f5716db7a5f5423d4f
Suhyeon Jang <suhy.jang.shj@gmail.com>
2022-03-14 00:22:44 +0900

    Flip board

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Keyboard from '../components/Keyboard';
-const guess = [
+const board = [
   ['', '', '', '', ''],
   ['', '', '', '', ''],
   ['', '', '', '', ''],
@@ -9,19 +9,96 @@ const guess = [
   ['', '', '', '', ''],
 ];
 
+let wordle = 'TRAIN';
+console.log(wordle);
+
 export default function Home() {
   const [point, setPoint] = useState({ row: 0, col: 0 });
-  // const [guess, setGuess] = useState(initialGuess);
+  const [word, setWord] = useState('');
+  const [gameOver, setGameOver] = useState(false);
+  const [message, setMessage] = useState('');
 
-  const handleKey = (key) => {
-    console.log('clicked', key);
-    if (point.col < 5) {
-      console.log(point);
-      setPoint({ ...point, col: point.col + 1 });
-      const { row, col } = point;
-      guess[row][col] = key;
+  const { row, col } = point;
+
+  const flipTile = () => {
+    const tiles = document.getElementById(`row-${row}`).childNodes;
+    const guessResults = [];
+
+    tiles.forEach((tile) => {
+      guessResults.push({ letter: tile.getAttribute('data'), color: 'gray' });
+    });
+
+    for (let c = 0; c < 5; c++) {
+      const tile = board[row][c];
+      // const tile = document.getElementById(`row-${row}`).childNodes;
+      if (tile === wordle[c]) {
+        guessResults[c].color = 'lime';
+      } else if (wordle.includes(tile)) {
+        guessResults[c].color = 'yellow';
+      }
+    }
+    tiles.forEach((tile, index) => {
+      setTimeout(() => {
+        const color = guessResults[index].color;
+        tile.classList.add(`bg-${color}-500`);
+      }, 500 * index);
+    });
+  };
+
+  const checkRow = () => {
+    if (col < 5) return;
+
+    flipTile();
+    if (wordle === word) {
+      setMessage('Super!');
+    } else if (row < 5) {
+      setPoint({ row: row + 1, col: 0 });
+    } else {
+      setGameOver(true);
+      setMessage('Game Over');
     }
   };
+
+  const deleteLetter = () => {
+    if (col > 0) {
+      board[row][col - 1] = '';
+      setWord(word.slice(0, col - 1));
+      const tile = document.getElementById(`row-${row}-col-${col - 1}`);
+      tile.setAttribute('data', '');
+      setPoint({ ...point, col: col - 1 });
+    }
+  };
+
+  const addLetter = (letter) => {
+    if (col < 5) {
+      board[row][col] = letter;
+      setWord(`${word}${letter}`);
+      const tile = document.getElementById(`row-${row}-col-${col}`);
+      tile.setAttribute('data', letter);
+      setPoint({ ...point, col: col + 1 });
+    }
+  };
+
+  const handleKey = (key) => {
+    if (gameOver) return;
+
+    if (key === 'ENTER') {
+      checkRow();
+    } else if (key === '←') {
+      deleteLetter();
+    } else if (col < 5) {
+      addLetter(key);
+    }
+  };
+
+  useEffect(() => {
+    if (message.length > 0) {
+      setTimeout(() => {
+        setMessage('');
+      }, 3000);
+    }
+  }, [message]);
+
   return (
     <div
       style={{
@@ -31,17 +108,24 @@ export default function Home() {
       <header className="">
         <h1>Wordle</h1>
       </header>
-      <div className="game w-100 mx-auto">
-        <div className="message-container"></div>
-        <div className="tile-container mx-auto w-[330px] mb-10">
-          {guess.map((row, rowIndex) => {
+      <div className="game w-100 mx-auto text-white">
+        <div className="message-container my-3 relative">
+          {message && (
+            <div className="rounded-[10px] bg-gray-500 w-fit mx-auto py-2 px-5 absolute left-0 right-0">
+              {message}
+            </div>
+          )}
+        </div>
+        <div className="board-container mx-auto w-[330px] mb-10">
+          {board.map((row, rowIndex) => {
             return (
-              <div key={rowIndex} className="flex">
+              <div key={rowIndex} id={`row-${rowIndex}`} className="flex">
                 {row.map((tile, colIndex) => {
                   return (
                     <div
                       key={`${rowIndex}-${colIndex}`}
-                      className="text-white tile w-[62px] h-[62px] border-2 box-border flex m-[2px] justify-center items-center"
+                      id={`row-${rowIndex}-col-${colIndex}`}
+                      className={`text-white tile w-[62px] h-[62px] border-2 box-border flex m-[2px] justify-center items-center`}
                     >
                       {tile}
                     </div>
