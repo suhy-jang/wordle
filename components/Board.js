45a6cb9919b6b28c5c2d04f0d7cf943a7db9abd1
Suhyeon Jang <suhy.jang.shj@gmail.com>
2022-03-14 22:38:20 +0900

    Add app icons

--- components/Board.js
+++ components/Board.js
@@ -1,6 +1,6 @@
 export default function Board({ board }) {
   return (
-    <div className="board-container mx-auto w-[210px] sm:w-[330px] my-5">
+    <div className="board-container mx-auto w-[260px] sm:w-[330px] my-5">
       {board.map((row, rowIndex) => {
         return (
           <div key={rowIndex} id={`row-${rowIndex}`} className="flex">
@@ -9,7 +9,7 @@ export default function Board({ board }) {
                 <div
                   key={`${rowIndex}-${colIndex}`}
                   id={`row-${rowIndex}-col-${colIndex}`}
-                  className={`tile rounded text-white tile w-[42px] h-[42px] sm:w-[62px] sm:h-[62px] border-2 box-border flex m-[2px] justify-center items-center`}
+                  className={`tile rounded text-white tile w-[52px] h-[52px] sm:w-[62px] sm:h-[62px] border-2 box-border flex m-[2px] justify-center items-center`}
                 >
                   {tile}
                 </div>
