f145c039cb4073841a5b618a1f422aaf4e4e84ac
Suhyeon Jang <suhy.jang.shj@gmail.com>
2022-03-16 22:13:00 +0900

    Fix keyboard align

--- components/Keyboard.js
+++ components/Keyboard.js
@@ -7,18 +7,18 @@ export default function Keyboard({ onClick, gameOver }) {
     ['ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '←'],
   ];
   return (
-    <div className="keyboard flex flex-col items-center">
+    <div className="flex flex-col items-center keyboard">
       {keys.map((keyRow, rowIndex) => (
         <div
           key={`keyboard-${rowIndex}`}
-          className="flex flex-row w-100 mt-0 mx-auto mb-2 touch-manipulation"
+          className="flex flex-row items-center justify-center mx-auto mt-0 mb-2 w-100 touch-manipulation"
         >
           {keyRow.map((key, colIndex) => (
             <button
               key={colIndex}
               data-key={key}
               onClick={() => onClick(key)}
-              className="key m-[2px] text-white text-sm sm:text-lg h-[42px] sm:h-[58px] px-2.5 sm:px-4 py-1 rounded flex justify-center items-center uppercase flex-1 bg-gray-500"
+              className="key m-[1px] sm:m-[1px] text-white text-sm sm:text-lg h-[42px] sm:h-[58px] px-2.5 sm:px-4 py-1 rounded flex justify-center items-center uppercase flex-1 bg-gray-500"
               disabled={!!gameOver}
             >
               {key === '←' ? <BackSlash /> : key}
